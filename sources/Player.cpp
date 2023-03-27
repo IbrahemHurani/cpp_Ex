479c61cd67b4e89f0b14cefacae9fa4bed5d3746
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2023-03-28 00:10:59 +0300

    Add files via upload

---
+++ sources/Player.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <stdexcept>
+#include "player.hpp"
+using namespace std;
+Player::Player(){
+
+}
+Player::Player(string name_player){
+	this->player_name=name_player;
+
+}
+string Player::getName(){
+    return this->player_name;
+
+}
+void Player::setName(string new_name){
+
+}
+int Player::cardesTaken(){
+	return 0;
+}
+int Player::stacksize(){
+	return 0;
+
+}
+bool Player::IsPlaying(){
+	return true;
+
+}
+void Player::setIsPlaying(bool Player_status){
+
+}
+
+
