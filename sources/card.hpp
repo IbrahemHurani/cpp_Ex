f6aef4d8acd887b1f66432756448060758de0351
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2023-03-27 23:16:20 +0300

    Add files via upload

---
+++ sources/card.hpp
@@ -0,0 +1,12 @@
+#ifndef CARD_HPP
+#define CARD_HPP
+namespace ariel{}
+    class Card{
+        private:
+            int Number_of_card;
+        public:
+            Card(int number_of_cards);
+            int cards_numbers();
+
+    };
+#endif
